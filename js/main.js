bf0e6c1dbffb46db296f316ccf713be1f9f0ea03
Marcelo Jimenez C <marcelojimenezcubillos@gmail.com>
2023-12-12 23:53:37 -0300

    Segunda Entrega - Filtro por categorias

---
+++ js/main.js
@@ -0,0 +1,459 @@
+const ropa = [
+    {
+        "tipo": "Camisa",
+        "precio": 63920,
+        "cantidad": 9,
+        "talla": "S",
+        "sexo": "Mujer",
+        "marca": "MarcaC"
+    },
+    {
+        "tipo": "Chaqueta",
+        "precio": 53035,
+        "cantidad": 66,
+        "talla": "M",
+        "sexo": "Hombre",
+        "marca": "MarcaA"
+    },
+    {
+        "tipo": "Vestido",
+        "precio": 108573,
+        "cantidad": 61,
+        "talla": "L",
+        "sexo": "Hombre",
+        "marca": "MarcaB"
+    },
+    {
+        "tipo": "Chaqueta",
+        "precio": 74074,
+        "cantidad": 39,
+        "talla": "S",
+        "sexo": "Mujer",
+        "marca": "MarcaD"
+    },
+    {
+        "tipo": "Chaqueta",
+        "precio": 16376,
+        "cantidad": 65,
+        "talla": "L",
+        "sexo": "Hombre",
+        "marca": "MarcaC"
+    },
+    {
+        "tipo": "Vestido",
+        "precio": 38087,
+        "cantidad": 4,
+        "talla": "S",
+        "sexo": "Hombre",
+        "marca": "MarcaA"
+    },
+    {
+        "tipo": "Camisa",
+        "precio": 93252,
+        "cantidad": 96,
+        "talla": "S",
+        "sexo": "Mujer",
+        "marca": "MarcaB"
+    },
+    {
+        "tipo": "Vestido",
+        "precio": 24005,
+        "cantidad": 84,
+        "talla": "S",
+        "sexo": "Mujer",
+        "marca": "MarcaD"
+    },
+    {
+        "tipo": "Camisa",
+        "precio": 52543,
+        "cantidad": 23,
+        "talla": "M",
+        "sexo": "Mujer",
+        "marca": "MarcaA"
+    },
+    {
+        "tipo": "Camisa",
+        "precio": 46726,
+        "cantidad": 37,
+        "talla": "L",
+        "sexo": "Mujer",
+        "marca": "MarcaA"
+    },
+    {
+        "tipo": "Falda",
+        "precio": 10065,
+        "cantidad": 84,
+        "talla": "M",
+        "sexo": "Mujer",
+        "marca": "MarcaB"
+    },
+    {
+        "tipo": "Chaqueta",
+        "precio": 11844,
+        "cantidad": 62,
+        "talla": "M",
+        "sexo": "Mujer",
+        "marca": "MarcaB"
+    },
+    {
+        "tipo": "Camisa",
+        "precio": 41326,
+        "cantidad": 73,
+        "talla": "XL",
+        "sexo": "Hombre",
+        "marca": "MarcaC"
+    },
+    {
+        "tipo": "Pantalon",
+        "precio": 46235,
+        "cantidad": 86,
+        "talla": "M",
+        "sexo": "Hombre",
+        "marca": "MarcaB"
+    },
+    {
+        "tipo": "Camisa",
+        "precio": 20818,
+        "cantidad": 47,
+        "talla": "XL",
+        "sexo": "Mujer",
+        "marca": "MarcaB"
+    },
+    {
+        "tipo": "Chaqueta",
+        "precio": 44961,
+        "cantidad": 12,
+        "talla": "L",
+        "sexo": "Mujer",
+        "marca": "MarcaA"
+    },
+    {
+        "tipo": "Vestido",
+        "precio": 72122,
+        "cantidad": 94,
+        "talla": "L",
+        "sexo": "Hombre",
+        "marca": "MarcaB"
+    },
+    {
+        "tipo": "Vestido",
+        "precio": 45321,
+        "cantidad": 75,
+        "talla": "L",
+        "sexo": "Hombre",
+        "marca": "MarcaC"
+    },
+    {
+        "tipo": "Chaqueta",
+        "precio": 69720,
+        "cantidad": 52,
+        "talla": "L",
+        "sexo": "Hombre",
+        "marca": "MarcaD"
+    },
+    {
+        "tipo": "Vestido",
+        "precio": 55458,
+        "cantidad": 41,
+        "talla": "M",
+        "sexo": "Hombre",
+        "marca": "MarcaB"
+    },
+    {
+        "tipo": "Falda",
+        "precio": 91727,
+        "cantidad": 91,
+        "talla": "M",
+        "sexo": "Mujer",
+        "marca": "MarcaB"
+    },
+    {
+        "tipo": "Camisa",
+        "precio": 64203,
+        "cantidad": 32,
+        "talla": "XL",
+        "sexo": "Mujer",
+        "marca": "MarcaC"
+    },
+    {
+        "tipo": "Pantalon",
+        "precio": 55071,
+        "cantidad": 82,
+        "talla": "XL",
+        "sexo": "Hombre",
+        "marca": "MarcaA"
+    },
+    {
+        "tipo": "Camisa",
+        "precio": 98099,
+        "cantidad": 9,
+        "talla": "S",
+        "sexo": "Mujer",
+        "marca": "MarcaB"
+    },
+    {
+        "tipo": "Falda",
+        "precio": 88636,
+        "cantidad": 91,
+        "talla": "S",
+        "sexo": "Mujer",
+        "marca": "MarcaA"
+    },
+    {
+        "tipo": "Camisa",
+        "precio": 35494,
+        "cantidad": 32,
+        "talla": "L",
+        "sexo": "Mujer",
+        "marca": "MarcaA"
+    },
+    {
+        "tipo": "Falda",
+        "precio": 43533,
+        "cantidad": 52,
+        "talla": "L",
+        "sexo": "Hombre",
+        "marca": "MarcaA"
+    },
+    {
+        "tipo": "Pantalon",
+        "precio": 10018,
+        "cantidad": 6,
+        "talla": "L",
+        "sexo": "Mujer",
+        "marca": "MarcaA"
+    },
+    {
+        "tipo": "Vestido",
+        "precio": 20778,
+        "cantidad": 15,
+        "talla": "L",
+        "sexo": "Mujer",
+        "marca": "MarcaD"
+    },
+    {
+        "tipo": "Falda",
+        "precio": 100422,
+        "cantidad": 1,
+        "talla": "L",
+        "sexo": "Hombre",
+        "marca": "MarcaD"
+    },
+    {
+        "tipo": "Chaqueta",
+        "precio": 70490,
+        "cantidad": 61,
+        "talla": "L",
+        "sexo": "Mujer",
+        "marca": "MarcaA"
+    },
+    {
+        "tipo": "Camisa",
+        "precio": 11564,
+        "cantidad": 37,
+        "talla": "XL",
+        "sexo": "Mujer",
+        "marca": "MarcaC"
+    },
+    {
+        "tipo": "Vestido",
+        "precio": 41873,
+        "cantidad": 42,
+        "talla": "M",
+        "sexo": "Mujer",
+        "marca": "MarcaB"
+    },
+    {
+        "tipo": "Chaqueta",
+        "precio": 59788,
+        "cantidad": 4,
+        "talla": "L",
+        "sexo": "Hombre",
+        "marca": "MarcaB"
+    },
+    {
+        "tipo": "Pantalon",
+        "precio": 21516,
+        "cantidad": 40,
+        "talla": "L",
+        "sexo": "Hombre",
+        "marca": "MarcaA"
+    },
+    {
+        "tipo": "Chaqueta",
+        "precio": 105819,
+        "cantidad": 90,
+        "talla": "S",
+        "sexo": "Hombre",
+        "marca": "MarcaC"
+    },
+    {
+        "tipo": "Chaqueta",
+        "precio": 27675,
+        "cantidad": 81,
+        "talla": "M",
+        "sexo": "Mujer",
+        "marca": "MarcaB"
+    },
+    {
+        "tipo": "Pantalon",
+        "precio": 74878,
+        "cantidad": 56,
+        "talla": "L",
+        "sexo": "Hombre",
+        "marca": "MarcaB"
+    },
+    {
+        "tipo": "Pantalon",
+        "precio": 84249,
+        "cantidad": 15,
+        "talla": "S",
+        "sexo": "Hombre",
+        "marca": "MarcaD"
+    },
+    {
+        "tipo": "Falda",
+        "precio": 95704,
+        "cantidad": 44,
+        "talla": "L",
+        "sexo": "Mujer",
+        "marca": "MarcaA"
+    },
+    {
+        "tipo": "Pantalon",
+        "precio": 93377,
+        "cantidad": 94,
+        "talla": "M",
+        "sexo": "Mujer",
+        "marca": "MarcaD"
+    },
+    {
+        "tipo": "Camisa",
+        "precio": 35177,
+        "cantidad": 21,
+        "talla": "XL",
+        "sexo": "Mujer",
+        "marca": "MarcaD"
+    },
+    {
+        "tipo": "Vestido",
+        "precio": 83771,
+        "cantidad": 85,
+        "talla": "M",
+        "sexo": "Mujer",
+        "marca": "MarcaB"
+    },
+    {
+        "tipo": "Falda",
+        "precio": 50029,
+        "cantidad": 97,
+        "talla": "L",
+        "sexo": "Mujer",
+        "marca": "MarcaC"
+    },
+    {
+        "tipo": "Falda",
+        "precio": 63089,
+        "cantidad": 77,
+        "talla": "S",
+        "sexo": "Mujer",
+        "marca": "MarcaD"
+    },
+    {
+        "tipo": "Falda",
+        "precio": 14355,
+        "cantidad": 67,
+        "talla": "S",
+        "sexo": "Mujer",
+        "marca": "MarcaC"
+    },
+    {
+        "tipo": "Falda",
+        "precio": 18259,
+        "cantidad": 76,
+        "talla": "XL",
+        "sexo": "Hombre",
+        "marca": "MarcaC"
+    },
+    {
+        "tipo": "Camisa",
+        "precio": 74197,
+        "cantidad": 95,
+        "talla": "S",
+        "sexo": "Hombre",
+        "marca": "MarcaD"
+    },
+    {
+        "tipo": "Vestido",
+        "precio": 76969,
+        "cantidad": 91,
+        "talla": "S",
+        "sexo": "Mujer",
+        "marca": "MarcaB"
+    },
+    {
+        "tipo": "Falda",
+        "precio": 82466,
+        "cantidad": 39,
+        "talla": "XL",
+        "sexo": "Mujer",
+        "marca": "MarcaD"
+    }
+
+];
+
+class RopaAlmacen {
+    constructor(ropa) {
+        this.ropa = ropa;
+    }
+
+    filtrar(criterio, valor) {
+
+        const resultado = this.ropa.filter(item => {
+            if (criterio === 'marca') {
+                return item.marca === valor;
+            } else if (criterio === 'precio') {
+                return item.precio >= Number(valor);
+            } else if (criterio === 'tipo') {
+                return item.tipo.toLowerCase() === valor.toLowerCase();
+            } else if (criterio === 'talla') {
+                return item.talla === valor.toUpperCase();
+            } else if (criterio === 'sexo'){
+                return item.sexo === valor.toLowerCase();
+            } else {
+                return item;
+            }
+        })
+
+        if (resultado.length > 0) {
+            return resultado;                                      
+        } else {
+            alert("No se encontraron ropas con esas caracteristicas");
+        }
+    }
+
+    salida(criterio, valor){
+        const resultadoFiltrado = this.filtrar(criterio, valor);
+        const resultDiv = document.getElementById("resultado");
+                
+        resultDiv.innerHTML = "<ul>";
+            resultadoFiltrado.forEach(item => {
+                resultDiv.innerHTML += `<li>${item.tipo} - ${item.precio}- ${item.cantidad}- ${item.talla}- ${item.sexo}- ${item.marca}</li>`;
+            });
+            resultDiv.innerHTML += "</ul>";
+        
+    }
+}
+
+const criterio          = prompt('Ingrese el criterio de filtrado (marca, precio, tipo, talla, sexo):');
+const valor             = prompt('Ingrese el valor a filtrar:');
+
+const almacen           = new RopaAlmacen(ropa);
+console.log(almacen.salida(criterio, valor));
+/*
+const almacen           = new RopaAlmacen(ropa);
+const resultadoFiltrado = almacen.filtrar(criterio, valor);
+const resultDiv         = document.getElementById("resultado");
+                      
+console.table(almacen.salida);
+*/
